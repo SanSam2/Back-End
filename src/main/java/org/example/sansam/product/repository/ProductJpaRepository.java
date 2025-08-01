393c512ffdd21c34e4b1c357bed9719024154cc7
eunkyung <melon0802@naver.com>
2025-08-01 15:09:35 +0900

    Refactor: productRepo 수정

--- src/main/java/org/example/sansam/product/repository/ProductJpaRepository.java
+++ src/main/java/org/example/sansam/product/repository/ProductJpaRepository.java
@@ -1,7 +1,9 @@
 package org.example.sansam.product.repository;
 
+import org.example.sansam.product.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductJpaRepository {
+public interface ProductJpaRepository extends JpaRepository<Product, Long> {
 }
