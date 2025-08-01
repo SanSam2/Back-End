64061ad707db479611a64740a61dd9feaac0d888
eunkyung <melon0802@naver.com>
2025-08-01 14:55:37 +0900

    Docs: aws 설정 추가

--- src/main/java/org/example/sansam/product/repository/ProductOptionJpaRepository.java
+++ src/main/java/org/example/sansam/product/repository/ProductOptionJpaRepository.java
@@ -1,6 +1,5 @@
 package org.example.sansam.product.repository;
 
-import org.example.sansam.product.domain.Product;
 import org.example.sansam.product.domain.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
