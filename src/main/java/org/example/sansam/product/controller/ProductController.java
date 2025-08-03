5b7c8425cb8a84fe4b38fb177a9199019b64af73
eunkyung <melon0802@naver.com>
2025-08-03 21:40:31 +0900

    Feat: 상품 상세 조회 API 구현
    상품 리스트에서 상품 클릭시 기본 옵션으로 상품 조회 기능 구현

--- src/main/java/org/example/sansam/product/controller/ProductController.java
+++ src/main/java/org/example/sansam/product/controller/ProductController.java
@@ -13,54 +13,53 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/products")
 public class ProductController {
 
-//    private final ProductService productService;
-//
-//    // 상품 품절 처리
-//    @PatchMapping ("/soldout")
-//    public ResponseEntity<?> markSoldOut(@RequestBody SoldoutRequest soldoutRequest) {
-//        try {
-//            SoldoutResponse response = new SoldoutResponse();
-//            return ResponseEntity.ok(response);
-//        } catch (Exception e) {
-//            return ResponseEntity.status(400).body(e.getMessage());
-//        }
-//    }
-//
-//    //상품 재고 조회 - 품절 시 반환값 ㅒ
-//    @GetMapping("/search-stock")
-//    public ResponseEntity<?> searchStock(@RequestBody SearchStockRequest searchStockRequest) {
-//        try {
-//            SearchStockResponse response = new SearchStockResponse();
-//            return ResponseEntity.ok(response);
-//        } catch (Exception e) {
-//            return ResponseEntity.status(400).body(e.getMessage());
-//        }
-//    }
-//
-//
-//    //상품 재고 차감
-//    @PatchMapping("/decrease-stock")
-//    public ResponseEntity<?> decreaseStock(@RequestBody DecreaseStockRequest decreaseStockRequest) {
-//        try {
-//            SearchStockRequest response = new SearchStockRequest();
-//            return ResponseEntity.ok(response);
-//        } catch (Exception e) {
-//            return ResponseEntity.status(400).body(e.getMessage());
-//        }
-//    }
-//
-//    //상품 상세 조회
-//    @GetMapping("/{productId}")
-//    public ResponseEntity<ProductResponse> getDefaultOption(@PathVariable Long productId) {
-//        return ResponseEntity.ok(productService.getDefaultOption(productId));
-//    }
-//
+    private final ProductService productService;
+    // 상품 품절 처리
+    @PatchMapping ("/soldout")
+    public ResponseEntity<?> markSoldOut(@RequestBody SoldoutRequest soldoutRequest) {
+        try {
+            SoldoutResponse response = new SoldoutResponse();
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body(e.getMessage());
+        }
+    }
+
+    //상품 재고 조회 - 품절 시 반환값 ㅒ
+    @GetMapping("/search-stock")
+    public ResponseEntity<?> searchStock(@RequestBody SearchStockRequest searchStockRequest) {
+        try {
+            SearchStockResponse response = new SearchStockResponse();
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body(e.getMessage());
+        }
+    }
+
+
+    //상품 재고 차감
+    @PatchMapping("/decrease-stock")
+    public ResponseEntity<?> decreaseStock(@RequestBody DecreaseStockRequest decreaseStockRequest) {
+        try {
+            SearchStockRequest response = new SearchStockRequest();
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body(e.getMessage());
+        }
+    }
+
+    //상품 상세 조회
+    @PostMapping("/{productId}")
+    public ResponseEntity<ProductResponse> getDefaultOption(@PathVariable Long productId, @RequestBody SearchRequest searchRequest) {
+        return ResponseEntity.ok(productService.getProduct(productId, searchRequest));
+    }
+
 //    @GetMapping("/{productId}/option")
 //    public ResponseEntity<ProductDetailResponse> getColorOption(
 //            @PathVariable Long productId,
 //            @RequestParam String color
 //    ) {
-//        return ResponseEntity.ok(productService.getOptionByColor(productId, color));
+//        return ResponseEntity.ok(productService.getProductByColor(productId, color));
 //    }
 
 }
--- src/main/java/org/example/sansam/wish/repository/WishJpaRepository.java
+++ src/main/java/org/example/sansam/wish/repository/WishJpaRepository.java
@@ -4,7 +4,9 @@ import org.example.sansam.wish.domain.Wish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WishJpaRepository extends JpaRepository<Wish, Long> {
-    Wish findByUserIdAndProductId(Long userId, Long productId);
+    Optional<Wish> findByUserIdAndProductId(Long userId, Long productId);
 }
