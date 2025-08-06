f7f19a1bda2a57a2397f84792bcec0c448849d87
hanseop9991 <fghjk000@naver.com>
2025-08-06 16:45:17 +0900

    Fix: 페이징 코드 수정

--- src/main/java/org/example/sansam/chat/config/WebSocketConfig.java
+++ src/main/java/org/example/sansam/chat/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package org.example.sansam.chat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
@@ -8,10 +10,13 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -26,7 +31,24 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
                 .setAllowedOriginPatterns("*")
-                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                .addInterceptors(new HttpSessionHandshakeInterceptor() {
+                    @Override
+                    public boolean beforeHandshake(ServerHttpRequest request,
+                                                   ServerHttpResponse response,
+                                                   WebSocketHandler wsHandler,
+                                                   Map<String, Object> attributes) throws Exception {
+                        System.out.println("[WebSocket] 핸드쉐이크 시작: " + request.getURI());
+                        return super.beforeHandshake(request, response, wsHandler, attributes);
+                    }
+                    @Override
+                    public void afterHandshake(ServerHttpRequest request,
+                                               ServerHttpResponse response,
+                                               WebSocketHandler wsHandler,
+                                               Exception ex) {
+                        System.out.println("[WebSocket] 핸드쉐이크 완료: " + request.getURI());
+                        super.afterHandshake(request, response, wsHandler, ex);
+                    }
+                })
                 .withSockJS();
     }
 
