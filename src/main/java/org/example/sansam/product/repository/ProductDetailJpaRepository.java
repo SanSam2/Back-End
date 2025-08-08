c535ce4a01e9d23e88eb5521753a715e4fe6fd14
eunkyung <melon0802@naver.com>
2025-08-08 17:44:42 +0900

    Fix: 상품 상세 조회 오류 수정
    ✅ 상품 상세 조회 오류 수정

--- src/main/java/org/example/sansam/product/repository/ProductDetailJpaRepository.java
+++ src/main/java/org/example/sansam/product/repository/ProductDetailJpaRepository.java
@@ -3,6 +3,8 @@ package org.example.sansam.product.repository;
 import org.example.sansam.product.domain.Product;
 import org.example.sansam.product.domain.ProductDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,13 @@ import java.util.List;
 @Repository
 public interface ProductDetailJpaRepository extends JpaRepository<ProductDetail,Long> {
     List<ProductDetail> findByProduct(Product product);
+
+    @Query("""
+    select distinct pd from ProductDetail pd
+    left join fetch pd.productConnects pc
+    left join fetch pc.option
+    where pd.product = :product
+""")
+    List<ProductDetail> findByProductWithConnects(@Param("product") Product product);
+
 }
