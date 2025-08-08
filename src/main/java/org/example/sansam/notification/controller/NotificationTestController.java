70eba5145767ce2a890550df0362b5f1c47f4926
윤호상 <hosang@yunhosang-ui-MacBookPro.local>
2025-08-08 17:41:19 +0900

    Fix : 채팅 이벤트 로직 수정
    # Conflicts:
    #	.idea/misc.xml
    #	src/main/java/org/example/sansam/chat/domain/ChatMember.java
    #	src/main/java/org/example/sansam/chat/domain/ChatMemberId.java
    #	src/main/java/org/example/sansam/chat/domain/ChatRoom.java
    #	src/main/java/org/example/sansam/chat/repository/ChatMemberRepository.java
    #	src/main/java/org/example/sansam/chat/repository/ChatRoomRepository.java

--- src/main/java/org/example/sansam/notification/controller/NotificationTestController.java
+++ src/main/java/org/example/sansam/notification/controller/NotificationTestController.java
@@ -2,6 +2,9 @@ package org.example.sansam.notification.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.sansam.chat.domain.ChatRoom;
+import org.example.sansam.chat.repository.ChatMemberRepository;
+import org.example.sansam.chat.repository.ChatRoomRepository;
 import org.example.sansam.notification.event.*;
 import org.example.sansam.product.domain.ProductDetail;
 import org.example.sansam.product.repository.ProductDetailJpaRepository;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -24,29 +29,38 @@ public class NotificationTestController {
     private final ApplicationEventPublisher publisher;
     private final ProductDetailJpaRepository productDetailJpaRepository;
     private final ProductJpaRepository productJpaRepository;
+    private final ChatMemberRepository chatMemberRepository;
+    private final ChatRoomRepository chatRoomRepository;
 
     @PostMapping("/send")
     public ResponseEntity<?> sendTestNotification() {
         try {
-            String email = "qwe123@naver.com";
-            User user = userRepository.findByEmail(email).orElseThrow();
-            log.info("user: {}", user.getName());
+
+
 //            String orderName = "호상상 외 2건";
 //            String productName = "유노상반팔";
 //            String senderName = "김김김";
 //            String message = "집에 가고 싶구나..";
 
-            ProductDetail productDetail = productDetailJpaRepository.findById(1L).orElseThrow();
+//
+//            ProductDetail productDetail = productDetailJpaRepository.findById(1L).orElseThrow();
+//
+//            Long beforeQuantity = productDetail.getQuantity();
+//            log.info("beforeQuantity: {}", beforeQuantity);
+//            Long orderQuantity = 5L;
+//            Long afterQuantity = beforeQuantity - orderQuantity;
+//            log.info("afterQuantity: {}", afterQuantity);
+//
+//            if (beforeQuantity > 50L && afterQuantity <= 50L) {
+//                publisher.publishEvent(new ProductQuantityLowEvent(productDetail));
+//            }
 
-            Long beforeQuantity = productDetail.getQuantity();
-            log.info("beforeQuantity: {}", beforeQuantity);
-            Long orderQuantity = 5L;
-            Long afterQuantity = beforeQuantity - orderQuantity;
-            log.info("afterQuantity: {}", afterQuantity);
 
-            if (beforeQuantity > 50L && afterQuantity <= 50L) {
-                publisher.publishEvent(new ProductQuantityLowEvent(productDetail));
-            }
+
+            ChatRoom chatRoom = chatRoomRepository.findById(1L).orElseThrow();
+            User sender = userRepository.findById(1L).orElseThrow();
+            log.info("chatRoom - {}", chatRoom);
+            publisher.publishEvent(new ChatEvent(chatRoom, sender,"이게 바로 테스트 성공"));
 
             return ResponseEntity.ok().build();
         }catch (Exception e) {
