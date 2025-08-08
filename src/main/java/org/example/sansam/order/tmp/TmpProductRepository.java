528efaa69d9ad9e5d176238a5403ae639acd3d85
xeulbn <gsbtiger0215@gmail.com>
2025-08-08 14:16:07 +0900

    FEAT : 상품과 주문의 연결

--- src/main/java/org/example/sansam/order/tmp/TmpProductRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.sansam.order.tmp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface TmpProductRepository extends JpaRepository<TmpProducts, Long> {
-    Optional<TmpProducts> findById(Long id);
-}
