d04a509e76a983283bc8fbcca582c0bf230cb471
윤호상 <hosang@yunhosang-ui-MacBookPro.local>
2025-08-11 13:12:04 +0900

    Fix : swagger 추가 위한 커밋 pull

--- src/main/java/org/example/sansam/order/domain/OrderProduct.java
+++ src/main/java/org/example/sansam/order/domain/OrderProduct.java
@@ -3,6 +3,7 @@ package org.example.sansam.order.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import org.example.sansam.product.domain.Product;
 import org.example.sansam.status.domain.Status;
 
