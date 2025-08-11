d9ab8f8e7aa5ec357bdea206e22185b35f941760
hanseop9991 <fghjk000@naver.com>
2025-08-11 16:53:27 +0900

    feat: 스웨거 api 명세서 추가

--- src/main/java/org/example/sansam/chat/controller/ChatMemberController.java
+++ src/main/java/org/example/sansam/chat/controller/ChatMemberController.java
@@ -1,9 +1,16 @@
 package org.example.sansam.chat.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.example.sansam.chat.dto.LastReadDTO;
 import org.example.sansam.chat.service.ChatMemberService;
+import org.example.sansam.order.dto.OrderResponse;
 import org.example.sansam.user.dto.LoginResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,6 +42,16 @@ public class ChatMemberController {
     }
 
     // 새로고침시 읽은날짜 로드
+    @Operation(summary = "읽은 날짜 수정", description = "해당 방의 마지막 읽은 날짜를 수정합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "날짜 수정 성공",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(value = "{}")  // 응답 본문 없음 표시
+                    )),
+            @ApiResponse(responseCode = "401", description = "로그인 필요",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(value = "{\"message\": \"마지막읽은 날짜를 수정하려면 로그인이 필요합니다.\"}")))
+    })
     @PutMapping("/last_read")
     public ResponseEntity<Void> lastRead(
             @RequestBody LastReadDTO dto,
