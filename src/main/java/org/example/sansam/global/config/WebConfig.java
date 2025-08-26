f29cc56da4f31134742a367d10ad368ccdfbd2c1
hosang <dvbf@naver.com>
2025-08-26 20:44:33 +0900

    Fix : cors 문제 수정

--- src/main/java/org/example/sansam/global/config/WebConfig.java
+++ src/main/java/org/example/sansam/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:3000",
+                        "https://ordefirenz.shop",
                         "https://front-end-fawn-pi.vercel.app",
                         "https://front-end-git-*-rlagusrud990619-9353s-projects.vercel.app"
                 )
