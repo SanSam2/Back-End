9cc6f2dca8bd0d502cd3c9747fc7036e8b66ebf2
hosang <dvbf@naver.com>
2025-08-27 10:23:16 +0900

    Fix:cors 문제 해결

--- src/main/java/org/example/sansam/global/config/WebConfig.java
+++ src/main/java/org/example/sansam/global/config/WebConfig.java
@@ -1,20 +1,21 @@
 package org.example.sansam.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(
-                        "https://ordefirenz.shop", // 도메인 주소
-                        "https://front-end-fawn-pi.vercel.app",
-                        "https://front-end-git-*-rlagusrud990619-9353s-projects.vercel.app"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedOriginPatterns(allowedOrigins)
+                .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
